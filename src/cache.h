4d66ef9d4d06b6271d56429a8c4933a3090897c8
dundas selzar <moddington@gmail.com>
2011-09-08 14:40:15 -0600

    Added interface for class Cache.

--- src/cache.h
+++ src/cache.h
@@ -1,8 +1,22 @@
 #ifndef CACHE_H
 #define CACHE_H
 
+#include <string>
+
+struct Comic;
+
 class Cache
 {
+	// Ctor
+	public:
+		explicit Cache(const std::string&);
+
+	// Cache
+	public:
+		Comic* readComicConfig(const std::string&) const;
+		void writeComicConfig(const Comic&);
+	private:
+		const std::string cache_dir;
 };
 
 #endif // CACHE_H
