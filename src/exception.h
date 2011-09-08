d883f2ed19b8ada100d74cba7603ed186cd87636
dundas selzar <moddington@gmail.com>
2011-09-08 15:20:39 -0600

    Added exception framework.

---
+++ src/exception.h
@@ -0,0 +1,34 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class Exception: public std::exception
+{
+  // Ctor
+  public:
+    explicit Exception(const std::string&) throw();
+    virtual ~Exception() throw();
+
+  // What
+  public:
+    virtual const char* what() const throw();
+  private:
+    std::string what_str;
+};
+
+#define EXCEPTION_DEF(name, args...) \
+  class name: public Exception       \
+  {                                  \
+    explicit name(args) throw();     \
+  };
+
+#define EXCEPTION_CTOR(name, what, args...) \
+  name::name(args) throw():                 \
+    Exception(what)                         \
+  {                                         \
+  }
+
+#endif // EXCEPTION_H
+
