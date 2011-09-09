d9e832808b66e92227cc19054d618454ae33a34e
dundas selzar <moddington@gmail.com>
2011-09-08 19:06:52 -0600

    Added implementation for class Cache's methods so far.

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,1 +1,169 @@
 #include "cache.h"
+
+#include <sqlite3.h>
+
+#include "comic.h"
+
+#define CACHE_DB       "cache.db"
+#define COMIC_SQL_COLS "'base_url','first_url','img_regex','next_regex'"
+#define CONFIG_TABLE   "comic-configs"
+
+
+// --------------------------------------------------------------------------------
+//  Helper classes
+// --------------------------------------------------------------------------------
+
+class SQLite3Db
+{
+  // Exceptions
+  public:
+    EXCEPTION(E_OpenFailed, "Could not open database file: " + config_file + "; " + details, const std::string& config_file, const std::string& details);
+
+  // Ctor+dtor
+  public:
+    SQLite3Db(const std::string& config_file, int flags) throw(E_OpenFailed):
+      db(NULL)
+    {
+      if (sqlite3_open_v2(config_file.c_str(), &db, flags, NULL) != SQLITE_OK)
+      {
+        std::string details(sqlite3_errmsg(db));
+        sqlite3_close(db);
+        throw E_OpenFailed(config_file, details);
+      }
+    }
+    ~SQLite3Db() throw()
+    {
+      sqlite3_close(db);
+    }
+
+  // Database
+  public:
+    operator sqlite3*() throw()
+    {
+      return db;
+    }
+    operator const sqlite3*() const throw()
+    {
+      return db;
+    }
+  private:
+    sqlite3* db;
+};
+
+class SQLite3Stmt
+{
+  // Exceptions
+  public:
+    EXCEPTION(E_PrepareFailed, details, const std::string& details);
+    EXCEPTION(E_StepFailed, details, const std::string& details);
+
+  // Ctor+dtor
+  public:
+    SQLite3Stmt(SQLite3Db& db, const std::string& statement) throw(E_PrepareFailed):
+      db(db),
+      stmt(NULL)
+    {
+      if (sqlite3_prepare_v2(db, statement.c_str(), -1, &stmt, NULL) != SQLITE_OK)
+        throw E_PrepareFailed(sqlite3_errmsg(db));
+    }
+    ~SQLite3Stmt() throw()
+    {
+      sqlite3_finalize(stmt);
+    }
+
+  // Statement
+  public:
+    operator sqlite3_stmt*() throw()
+    {
+      return stmt;
+    }
+    operator const sqlite3_stmt*() const throw()
+    {
+      return stmt;
+    }
+    bool step() const throw(E_StepFailed)
+    {
+      switch (sqlite3_step(stmt))
+      {
+        case SQLITE_OK:
+          return true;
+        case SQLITE_DONE:
+          return false;
+        default:
+          throw E_StepFailed(sqlite3_errmsg(db));
+      }
+    }
+  private:
+    sqlite3* db;
+    sqlite3_stmt* stmt;
+};
+
+
+// --------------------------------------------------------------------------------
+//  Exceptions
+// --------------------------------------------------------------------------------
+
+Cache::EXCEPTION_CTOR(E_ConfigDbOpenFailed, "Could not open config database file '" + config_file + "'; " + details, const std::string& config_file, const std::string& details)
+Cache::EXCEPTION_CTOR(E_ConfigDbStmtFailed, "Statement '" + statement + "' on config database failed; " + details, const std::string& statement, const std::string& details)
+Cache::EXCEPTION_CTOR(E_NoComicConfigFound, "Comic '" + comic_name + "' does not exist in database" , const std::string& comic_name)
+
+
+// --------------------------------------------------------------------------------
+//  Ctor
+// --------------------------------------------------------------------------------
+
+Cache::Cache(const std::string& cache_dir) throw():
+  cache_dir(cache_dir)
+{
+}
+
+
+// --------------------------------------------------------------------------------
+//  Cache
+// --------------------------------------------------------------------------------
+
+Comic* Cache::readComicConfig(const std::string& comic_name) const throw(E_ConfigDbOpenFailed, E_ConfigDbStmtFailed, E_NoComicConfigFound)
+{
+  Comic comic;
+  std::string db_path = cache_dir + '/' + CACHE_DB;
+  std::string stmt_str = "select " COMIC_SQL_COLS " from `" CONFIG_TABLE "` where `name` = `" + comic_name + "`;";
+
+  try
+  {
+    SQLite3Db db(db_path, SQLITE_OPEN_READONLY);
+    // TODO - schema check
+    SQLite3Stmt stmt(db, stmt_str);
+    if (!stmt.step())
+      throw E_NoComicConfigFound(comic_name);
+
+    comic.base_url.assign((const char*)sqlite3_column_text(stmt, 0));
+    comic.first_url.assign((const char*)sqlite3_column_text(stmt, 1));
+    comic.img_regex.assign((const char*)sqlite3_column_text(stmt, 2));
+    comic.next_regex.assign((const char*)sqlite3_column_text(stmt, 3));
+  }
+  catch (SQLite3Db::E_OpenFailed e)
+  { throw E_ConfigDbOpenFailed(db_path,  e.what()); }
+  catch (SQLite3Stmt::E_PrepareFailed e)
+  { throw E_ConfigDbStmtFailed(stmt_str, e.what()); }
+  catch (SQLite3Stmt::E_StepFailed e)
+  { throw E_ConfigDbStmtFailed(stmt_str, e.what()); }
+
+  return new Comic(comic);
+}
+
+void Cache::writeComicConfig(const Comic& comic) throw(E_ConfigDbOpenFailed, E_ConfigDbStmtFailed)
+{
+  std::string db_path = cache_dir + '/' + CACHE_DB;
+  std::string stmt_str = "insert into `" CONFIG_TABLE "` (" COMIC_SQL_COLS ") values (" + comic.getSQLValuesStr() + ")";
+
+  try
+  {
+    SQLite3Db db(db_path, SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
+    // TODO - schema check
+    SQLite3Stmt stmt(db, stmt_str);
+  }
+  catch (SQLite3Db::E_OpenFailed e)
+  { throw E_ConfigDbOpenFailed(db_path, e.what()); }
+  catch (SQLite3Stmt::E_PrepareFailed e)
+  { throw E_ConfigDbStmtFailed(stmt_str, e.what()); }
+}
--- src/cache.h
+++ src/cache.h
@@ -3,20 +3,28 @@
 
 #include <string>
 
+#include "exception.h"
+
 struct Comic;
 
 class Cache
 {
-	// Ctor
-	public:
-		explicit Cache(const std::string&) throw();
+  // Exceptions
+  public:
+    EXCEPTION_DEF(E_ConfigDbOpenFailed, const std::string&, const std::string&);
+    EXCEPTION_DEF(E_ConfigDbStmtFailed, const std::string&, const std::string&);
+    EXCEPTION_DEF(E_NoComicConfigFound, const std::string&);
 
-	// Cache
-	public:
-		Comic* readComicConfig(const std::string&) const throw(E_ConfigDbLocked);
-		void writeComicConfig(const Comic&) throw(E_ConfigDbLocked);
-	private:
-		const std::string cache_dir;
+  // Ctor
+  public:
+    explicit Cache(const std::string&) throw();
+
+  // Cache
+  public:
+    Comic* readComicConfig(const std::string&) const throw(E_ConfigDbOpenFailed, E_ConfigDbStmtFailed, E_NoComicConfigFound);
+    void writeComicConfig(const Comic&) throw(E_ConfigDbOpenFailed, E_ConfigDbStmtFailed);
+  private:
+    const std::string cache_dir;
 };
 
 #endif // CACHE_H
