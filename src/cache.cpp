cb6ae11463d10e8e4404d9f3704222cbe49b5256
dundas selzar <moddington@gmail.com>
2011-09-09 12:53:21 -0600

    Removed superfluous #define.

--- src/cache.cpp
+++ src/cache.cpp
@@ -5,11 +5,10 @@
 
 #include "comic.h"
 
-#define CACHE_DB           "cache.db"
-#define COMIC_TABLE        "pages"
-#define COMIC_SCHEMA       "(`comic_name` TEXT)"
-#define CONFIG_TABLE       "configs"
-#define CONFIG_SCHEMA      "(`name` TEXT PRIMARY KEY, `base_url` TEXT, `first_url` TEXT, `img_regex` TEXT, `next_regex` TEXT)"
+#define COMIC_TABLE   "pages"
+#define COMIC_SCHEMA  "(`comic_name` TEXT)"
+#define CONFIG_TABLE  "configs"
+#define CONFIG_SCHEMA "(`name` TEXT PRIMARY KEY, `base_url` TEXT, `first_url` TEXT, `img_regex` TEXT, `next_regex` TEXT)"
 
 
 // --------------------------------------------------------------------------------
