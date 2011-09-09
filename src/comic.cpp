d10b0da192bc10a4a09a6a2b5bd56bef10906463
dundas selzar <moddington@gmail.com>
2011-09-09 07:24:59 -0600

    Fixed incorrect quotation marks in SQL statements.

--- src/comic.cpp
+++ src/comic.cpp
@@ -1,6 +1,6 @@
 #include "comic.h"
 
-#define SQL_COLS "'base_url','first_url','img_regex','next_regex'"
+#define SQL_COLS "`base_url`,`first_url`,`img_regex`,`next_regex`"
 
 // --------------------------------------------------------------------------------
 //  SQL
@@ -8,7 +8,7 @@
 
 std::string Comic::getSQLInsertStr(const std::string& table_name, const std::string& comic_name) const throw()
 {
-  return "insert into `" + table_name + "` ('name'," SQL_COLS ") values ('" + comic_name + "','" + base_url + "','" + first_url + "','" + img_regex + "','" + next_regex + "');";
+  return "insert into `" + table_name + "` (`name`," SQL_COLS ") values ('" + comic_name + "','" + base_url + "','" + first_url + "','" + img_regex + "','" + next_regex + "');";
 }
 
 std::string Comic::getSQLSelectStr(const std::string& table_name, const std::string& comic_name) const throw()
