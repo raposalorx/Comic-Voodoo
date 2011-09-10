4ed1b68f967d59f50a6403f1b1e1f3d831d90d8b
dundas selzar <moddington@gmail.com>
2011-09-09 18:59:41 -0600

    Added notes to TODO in src/spider.cpp

--- src/spider.cpp
+++ src/spider.cpp
@@ -22,7 +22,10 @@ Spider::Spider(const std::string& picture_dir, const Comic& comic) throw():
 
 Strip* Spider::fetchNextStrip() throw(/* TODO */)
 {
-  // TODO
+  // TODO - should fetch the page at current_url, create a Strip object from the contents, download the comic images to picture_dir, and figure out what the next value of current_url should be
+  //      - should return NULL when there are no more strips to fetch; you'll probably have to change around the data used for update checks that is stored in struct Comic (currently only Comic::current_url is used for update checks)
+  //      - check src/cache.h and src/cache.cpp for examples of how the exception system works in this project
+  //      - also check docs/adding_columns for a list of places to update when adding members to struct Comic or struct Strip
   return NULL;
 }
 
