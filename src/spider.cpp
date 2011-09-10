f525f4da510fa985b802bf40cfa733a3657459bf
dundas selzar <moddington@gmail.com>
2011-09-09 22:18:47 -0600

    Added bare structure for class Spider.

--- src/spider.cpp
+++ src/spider.cpp
@@ -1,1 +1,28 @@
 #include "spider.h"
+
+
+// --------------------------------------------------------------------------------
+//  Ctor
+// --------------------------------------------------------------------------------
+
+Spider::Spider(const Comic& comic) throw():
+  comic(comic),
+  current_url(comic.current_url.empty() ? comic.first_url : comic.current_url)
+{
+}
+
+
+// --------------------------------------------------------------------------------
+//  Spidering
+// --------------------------------------------------------------------------------
+
+Strip* Spider::fetchNextStrip() throw(/* TODO */)
+{
+  // TODO
+  return NULL;
+}
+
+const std::string& Spider::getCurrentURL() const throw()
+{
+  return current_url;
+}
--- src/spider.h
+++ src/spider.h
@@ -1,8 +1,24 @@
 #ifndef SPIDER_H
 #define SPIDER_H
 
+#include <string>
+
+#include "comic.h"
+#include "strip.h"
+
 class Spider
 {
+  // Ctor
+  public:
+    explicit Spider(const Comic&) throw();
+
+  // Spidering
+  public:
+    Strip* fetchNextStrip() throw(/* TODO */);
+    const std::string& getCurrentURL() const throw();
+  private:
+    const Comic comic;
+    std::string current_url;
 };
 
 #endif // SPIDER_H
