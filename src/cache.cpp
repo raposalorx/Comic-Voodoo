865df05ec50a5803807abc0c93c7abee4d703636
dundas selzar <moddington@gmail.com>
2011-09-09 22:00:10 -0600

    Renamed database table for comics to 'comics'.

--- src/cache.cpp
+++ src/cache.cpp
@@ -5,7 +5,7 @@
 
 #include "comic.h"
 
-#define COMIC_TABLE   "pages"
+#define COMIC_TABLE   "comics"
 #define COMIC_SCHEMA  "(`comic_name` TEXT)"
 #define CONFIG_TABLE  "configs"
 #define CONFIG_SCHEMA "(`name` TEXT PRIMARY KEY, `base_url` TEXT, `first_url` TEXT, `img_regex` TEXT, `next_regex` TEXT)"
