a70c25f896cb565192135789d6c3ae98c3724d74
Brian lorx Robertson <raposa.msscripting@gmail.com>
2011-09-15 17:06:41 -0600

    Fixed comic not being a struct because I'm a moron.

--- src/comic.h
+++ src/comic.h
@@ -7,24 +7,6 @@
 
 struct Comic
 {
-    Comic(std::string cname, std::string cbase_url, std::string cfirst_url, std::string cimg_regex, std::string cnext_regex, std::string cend_on_url, unsigned short cread_end_url, unsigned short cdownload_imgs, unsigned short vmark, std::string vlast_url, unsigned short vlast_img, std::string vcurrent_url):
-        name(cname),
-        base_url(cbase_url), 
-        first_url(cfirst_url),
-        end_on_url(cend_on_url),
-        read_end_url(cread_end_url),
-        download_imgs(cdownload_imgs),
-
-        mark(vmark),
-        last_url(vlast_url),
-        last_img(vlast_img),
-        current_url(vcurrent_url),
-
-        img_regex(cimg_regex),
-        next_regex(cnext_regex)
-    {}
-
-    private:
     // config
     std::string name;
     std::string base_url;
